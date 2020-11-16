b5f8b00c3f3af35ed356c9b186150645500f1eca
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-11-16 16:32:44 +0100

    Feat: remove todo, return todo object in create function

--- services/TodoService.js
+++ services/TodoService.js
@@ -11,7 +11,7 @@ const TodoService = {
             if (!saved) {
                 return reject("Error occured while trying to save todo.")
             }
-            return resolve(true);
+            return resolve({todo: saved});
         } catch (err) {
             return reject(err);
         }
@@ -20,6 +20,19 @@ const TodoService = {
         try {
             const todos = await Todo.find({userId: userId});
             return resolve(todos);
+        } catch (err) {
+            return reject(err);
+        }
+    }),
+    removeTodo: (userId, todoId) => new Promise(async (resolve, reject) => {
+        try {
+            const todo = await Todo.findById(todoId);
+            if (todo.userId == userId) {
+                await todo.remove();
+            } else {
+                return reject("You are not authorized to delete this todo");
+            }
+            return resolve(true);
         } catch (err) {
             return reject(err);
         }
